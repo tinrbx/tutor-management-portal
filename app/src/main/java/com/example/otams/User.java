fd9fe9be669707ed9ea8462a5224e85e6f9611f5
Krish Shah <krishah247@gmail.com>
2025-10-04 17:00:41 -0400

    making user class

---
+++ app/src/main/java/com/example/otams/User.java
@@ -0,0 +1,22 @@
+package com.example.otams;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phoneNum;
+    private String userType;
+
+    public User(String firstName, String lastName, String email, String password, String phoneNum, String userType){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phoneNum = phoneNum;
+        this.userType = userType;
+    }
+
+
+}
