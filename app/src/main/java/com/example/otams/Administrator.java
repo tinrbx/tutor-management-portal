53b62bcf4b216026e0fe8d337b39546cd95d0a4f
Krish Shah <krishah247@gmail.com>
2025-10-11 23:44:40 -0400

    Created Admin class

---
+++ app/src/main/java/com/example/otams/Administrator.java
@@ -0,0 +1,10 @@
+package com.example.otams;
+
+public class Administrator extends User {
+
+    public Administrator(String firstName, String lastName, String email, String password, String phone) {
+        super(firstName, lastName, email, password, phone, "Administrator");
+    }
+
+}
+
