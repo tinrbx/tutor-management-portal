3887e37069140646ad6e898d3d38a7c17182bbb8
Nicolas Benavides <76823475+nicob1800@users.noreply.github.com>
2025-10-12 20:42:14 -0400

    Registration page is done except for registration button functionality. Just need to do the verification and then saving the information

--- app/src/main/java/com/example/otams/RegisterActivity.java
+++ app/src/main/java/com/example/otams/RegisterActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioGroup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -16,21 +17,32 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        ImageButton registerButton = findViewById(R.id.backButt);
+        ImageButton backButton = findViewById(R.id.backButt);
         RadioGroup radioGroup = findViewById(R.id.radioGroup);
         EditText program = findViewById(R.id.program);
         EditText highestLevelOfStudy = findViewById(R.id.highestLevelOfStudy);
         EditText coursesToTeach = findViewById(R.id.coursesToTeach);
-        if(radioGroup.getCheckedRadioButtonId() == R.id.Student) {
-            program.setVisibility(View.VISIBLE);
-            highestLevelOfStudy.setVisibility(View.GONE);
-            coursesToTeach.setVisibility(View.GONE);
-        }else if(radioGroup.getCheckedRadioButtonId() == R.id.Tutor){
-            program.setVisibility(View.GONE);
-            highestLevelOfStudy.setVisibility(View.VISIBLE);
-            coursesToTeach.setVisibility(View.VISIBLE);
-        }
-        registerButton.setOnClickListener(new View.OnClickListener() {
+
+        program.setVisibility(View.GONE);
+        highestLevelOfStudy.setVisibility(View.GONE);
+        coursesToTeach.setVisibility(View.GONE);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull RadioGroup group, int checkedId) {
+                if(checkedId == R.id.Student) {
+                    program.setVisibility(View.VISIBLE);
+                    highestLevelOfStudy.setVisibility(View.GONE);
+                    coursesToTeach.setVisibility(View.GONE);
+                }else if(checkedId == R.id.Tutor){
+                    program.setVisibility(View.GONE);
+                    highestLevelOfStudy.setVisibility(View.VISIBLE);
+                    coursesToTeach.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(RegisterActivity.this, WelcomeActivity.class);
