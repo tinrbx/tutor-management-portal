057248c58877616eeb861606fc862d42915e385b
Nicolas Benavides <76823475+nicob1800@users.noreply.github.com>
2025-11-09 23:01:55 -0500

    back button in CreateTimeslotActivity now works. NEED TO DO: in tutor dashboard page, we ned to put in the functionality fo rht pending requests panel

--- app/src/main/java/com/example/otams/CreateTimeslotActivity.java
+++ app/src/main/java/com/example/otams/CreateTimeslotActivity.java
@@ -1,6 +1,9 @@
 package com.example.otams;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,5 +11,14 @@ public class CreateTimeslotActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_timeslot);
+
+        ImageButton backButton = findViewById(R.id.backButt);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateTimeslotActivity.this, TutorDashboardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
